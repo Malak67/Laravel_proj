8ffca5426e4902bc95bad05a708fd06a5bd3f204
Florin Georgescu <fgeorgescu@ixiacom.com>
2015-01-21 17:55:32 +0200

    new changes, still at step 2.3 registering users

---
+++ makeitsnappy/app/controllers/questions.php
@@ -0,0 +1,16 @@
+<?php
+
+class Questions_Controller extends BaseController
+{
+	echo 'daadad';
+	public $restful = true;
+	
+	public function get_index()
+	{
+		// return View::make('questions.index')
+			// ->with('title', 'Make it Snappy Q&A - Home');
+		echo 'branza';
+	}
+}
+
+?>
---
+++ makeitsnappy/app/views/layouts/default.blade.php
@@ -0,0 +1,38 @@
+<!Doctype html>
+<html>
+	<head>
+		<title> {{ $title }}
+		{{ HTML::style('/css/main.css') }}
+	</head>
+	
+	<body>	
+		<div id="container">
+		
+			<div id="header">
+				{{ HTML::link('/', 'Make it Snappy Q&A') }}
+			</div><!-- end header -->
+			
+			<div id="nav">
+				<ul>
+					<li>{{ HTML::link('/', 'Home') }}</li>
+					<li>{{ HTML::link('/', 'Register') }}</li>
+					<li>{{ HTML::link('/', 'Login') }}</li>
+				</ul>
+			</div><!-- end nav -->
+			
+			<div id="content">
+				@if(Session::has('message'))
+					<p id="message">{{ Session::get('message') }}</p>
+				@endif
+				
+				@yield('content')
+			</div><!-- end content -->
+			
+			<div id="footer">
+				&copy; Make It Snappy Q&A {{ date('Y') }}
+			</div><!-- end footer -->
+			
+		</div>
+	</body>
+	
+</html>
